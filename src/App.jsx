57787643e8ffb0c807ae1fdbaa8f820cb2808bd8
agnaldo <agnaldopidev@gmail.com>
2023-04-21 09:01:42 -0300

    create sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,30 @@
-import { Header } from './Header';
-import './global.css';
+import { Header } from './components/Header';
+import { Sidebar } from './components/Sidebar';
+import { Post } from './components/Post';
 
+import './global.css';
+import styles from './App.module.css';
 
 function App() {
-  return (<Header />)
+  return (
+    <>
+      <Header />
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post
+            author="Agnaldo Correia"
+            content="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Maxime itaque quas corporis beatae veritatis, reprehenderit asperiores vitae quod possimus qui dignissimos unde deleniti consequatur quae, repellat debitis sunt, est rerum!"
+          />
+          <br />
+          <Post
+            author="Ana Beatriz"
+            content="Um novo post muito legal"
+          />
+        </main>
+      </div>
+    </>
+  )
 }
 
 export default App
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,12 @@
 import styles from './Header.module.css';
-import igniteLogon from './assets/logotipo.svg'
+import igniteLogon from './../assets/logotipo.svg'
 
 
 export function Header() {
     return (
         <header className={styles.header}>
             <img src={igniteLogon} alt="Logotipo Ignite" />
+            <strong>Blog do Dev</strong>
         </header>
     )
 }
